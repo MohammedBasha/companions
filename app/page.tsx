6aee808932bd9c40b5c736898492c47de5c63720
MohammedBasha <me@mohammedbasha.com>
2025-06-14 17:16:58 +0300

    Finishing thr Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,47 @@
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
     return (
-        <div>
-            <h1 className="text-2xl underline">Welcome to my Saas App</h1>
-            <Button>Let&apos;s get start</Button>
-        </div>
+        <main>
+            <h1 className="text-2xl underline">Popular Companions</h1>
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura the Brainy Explorer"
+                    topic="Neural Network of the Brain"
+                    subject="Science"
+                    duration={45}
+                    color="#E5D0FF"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Countsy the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    subject="Maths"
+                    duration={30}
+                    color="#FFDA6E"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Verba the Vocabulary Builder"
+                    topic="English Literature "
+                    subject="Language"
+                    duration={30}
+                    color="#BDE7FF"
+                />
+            </section>
+            <section className="home-section">
+                <CompanionsList
+                title="Recently completed lessons"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"/>
+
+                <CTA />
+            </section>
+        </main>
     );
 };
 
---
+++ components/CTA.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Cta() {
+    return (
+        <section className="cta-section">
+            <div className="cta-badge">Start learning your way</div>
+            <h2 className="text-3xl font-bold">
+                Build and personalize learning companion
+            </h2>
+            <p>Pick a name, subject, voice, & personality — and start learning through voice conversations that feel natural and fun.</p>
+            <Image src="images/cta.svg" alt="cta" width={300} height={300} />
+            <button className="btn-primary">
+                <Image src="/icons/plus.svg" alt="plus" width={12} height={12}/>
+                <Link href="/companions/new">
+                    <p>Build a new companion</p>
+                </Link>
+            </button>
+        </section>
+    );
+}
+
+export default Cta;
